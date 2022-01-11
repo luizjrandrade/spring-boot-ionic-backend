2cd15a7264cc5b80134be898e6d5c743060483e7
Luiz Junior <jr.luizandrade@gmail.com>
2022-01-10 22:07:14 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/luizjr/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/luizjr/cursomc/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.luizjr.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.luizjr.cursomc.domain.Categoria;
+import com.luizjr.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 }
