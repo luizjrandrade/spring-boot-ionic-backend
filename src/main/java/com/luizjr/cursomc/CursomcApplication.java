b95b3d6ca90be5e84d6bb01c22e0fb69642d874d
Luiz Junior <jr.luizandrade@gmail.com>
2022-01-15 12:38:51 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/luizjr/cursomc/CursomcApplication.java
+++ src/main/java/com/luizjr/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.luizjr.cursomc.domain.Categoria;
 import com.luizjr.cursomc.domain.Cidade;
+import com.luizjr.cursomc.domain.Cliente;
+import com.luizjr.cursomc.domain.Endereco;
 import com.luizjr.cursomc.domain.Estado;
 import com.luizjr.cursomc.domain.Produto;
+import com.luizjr.cursomc.domain.enums.TipoCliente;
 import com.luizjr.cursomc.repositories.CategoriaRepository;
 import com.luizjr.cursomc.repositories.CidadeRepository;
+import com.luizjr.cursomc.repositories.ClienteRepository;
+import com.luizjr.cursomc.repositories.EnderecoRepository;
 import com.luizjr.cursomc.repositories.EstadoRepository;
 import com.luizjr.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +32,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -67,6 +76,17 @@ public class CursomcApplication implements CommandLineRunner{
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
 		
+		// Clientes, endereços e telefone
+		Cliente cli1 = new Cliente(null, "Luiz Júnior", "luiz09@hotmail.com", "64274209172", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("63984095737","63222523212"));
+		
+		Endereco e1 = new Endereco(null, "706 Sul Al25", "13", "Casa 31", "Pds", "77022-400", cli1, cid1);
+		Endereco e2 = new Endereco(null, "Rua 8", "Tripoli", "Apto 306", "Centro", "77060-500", cli1, cid2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
---
+++ src/main/java/com/luizjr/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,42 @@
+package com.luizjr.cursomc.domain.enums;
+
+public enum TipoCliente {
+	
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		
+		//caso for nulo
+		if (cod == null) {
+			return null;
+		}
+		
+		// todo objeto X nos valores possiveis
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		//se esgotar o FOR, e não 1 ou 2, código invalido!
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+	
+}
