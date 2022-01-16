70416fee183bd7bcc102fa950843a4d212d03557
Luiz Junior <jr.luizandrade@gmail.com>
2022-01-16 15:51:30 -0300

    Listando todas as categorias

---
+++ src/main/java/com/luizjr/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,38 @@
+package com.luizjr.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.luizjr.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {
+	}
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getName();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
