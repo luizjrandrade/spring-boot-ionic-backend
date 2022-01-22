b2dcfbfd4b38943eb8dc0d4eb7b33d6a418f2609
Luiz Junior <jr.luizandrade@gmail.com>
2022-01-22 17:13:46 -0300

    validação sintática com Bean Validation

--- src/main/java/com/luizjr/cursomc/services/CategoriaService.java
+++ src/main/java/com/luizjr/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.luizjr.cursomc.domain.Categoria;
+import com.luizjr.cursomc.dto.CategoriaDTO;
 import com.luizjr.cursomc.repositories.CategoriaRepository;
 import com.luizjr.cursomc.services.exceptions.DataIntegrityException;
 import com.luizjr.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pagerequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pagerequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
