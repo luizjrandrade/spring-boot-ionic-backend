53486d27182d14e82c520f3064e7de2d521484bb
Luiz Junior <jr.luizandrade@gmail.com>
2022-07-25 10:40:56 -0300

    Email Html

--- src/main/java/com/luizjr/cursomc/services/EmailService.java
+++ src/main/java/com/luizjr/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.luizjr.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.luizjr.cursomc.domain.Pedido;
@@ -10,5 +12,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 
 }
