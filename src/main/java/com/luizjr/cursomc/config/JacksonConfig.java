e0466d7fe022ab468cc6de036a5d813e7ac2e383
Luiz Junior <jr.luizandrade@gmail.com>
2022-07-30 12:27:28 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/luizjr/cursomc/config/JacksonConfig.java
+++ src/main/java/com/luizjr/cursomc/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package com.luizjr.cursomc.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luizjr.cursomc.domain.PgtoComBoleto;
@@ -21,6 +23,11 @@ public class JacksonConfig {
 			};
 		};
 		return builder;
+	}
+	
+	@Bean
+	public JavaMailSender jMS (){
+		return new JavaMailSenderImpl();
 	}
 
 }
