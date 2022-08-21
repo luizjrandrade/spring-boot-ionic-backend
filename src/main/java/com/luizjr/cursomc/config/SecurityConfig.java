893790cc59e41e4d026c442bb29db5d9d06d4b3d
Luiz Junior <jr.luizandrade@gmail.com>
2022-08-21 15:07:33 -0300

    Implementando autorizacao

--- src/main/java/com/luizjr/cursomc/config/SecurityConfig.java
+++ src/main/java/com/luizjr/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.luizjr.cursomc.security.JWTAuthenticationFilter;
+import com.luizjr.cursomc.security.JWTAuthorizationFilter;
 import com.luizjr.cursomc.security.JWTUtil;
 
 @Configuration
@@ -59,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		//não cria sessão de usuários
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
