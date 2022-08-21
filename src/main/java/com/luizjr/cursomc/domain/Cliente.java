a4e33f674daa75c398780c729d4c6fb2841bee4d
Luiz Junior <jr.luizandrade@gmail.com>
2022-08-21 11:18:29 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/luizjr/cursomc/domain/Cliente.java
+++ src/main/java/com/luizjr/cursomc/domain/Cliente.java
@@ -153,7 +153,10 @@ public class Cliente implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -165,7 +168,12 @@ public class Cliente implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Cliente other = (Cliente) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
