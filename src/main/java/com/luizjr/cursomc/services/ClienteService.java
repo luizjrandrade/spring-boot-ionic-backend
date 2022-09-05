807697f22d060a7e94dec04a84961e77e8a53c22
Luiz Junior <jr.luizandrade@gmail.com>
2022-09-05 09:11:09 -0300

    Usando padrao para nomes de imagens

--- src/main/java/com/luizjr/cursomc/services/ClienteService.java
+++ src/main/java/com/luizjr/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.luizjr.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -43,6 +45,11 @@ public class ClienteService {
 	@Autowired
 	private S3Service s3Service;
 	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	public Cliente find(Integer id) {
 		
@@ -121,12 +128,11 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado.");
 		}
 		
-		URI uri = s3Service.uploadFile(multipartFile);
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 		
-		Optional<Cliente> cli = repo.findById(user.getId());
-		cli.orElse(null).setImageUrl(uri.toString());
-		repo.save(cli.orElse(null));
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
+		//monta prefixo do arquivo
 		
-		return uri;
-	}
+		}
 }
